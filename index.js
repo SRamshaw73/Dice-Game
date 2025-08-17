a92c9d2fb5c3bfe165cc090979e98e2b72fefded
SRamshaw73 <114100998+SRamshaw73@users.noreply.github.com>
2025-08-17 11:44:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const randomNumber1=(Math.floor(Math.random()*6))+1;
+const randomNumber2=(Math.floor(Math.random()*6))+1;
+document.querySelector(".img1").setAttribute("src", `./images/dice${randomNumber1}.png`);
+document.querySelector(".img2").setAttribute("src", `./images/dice${randomNumber2}.png`);
+
+if (randomNumber1>randomNumber2){
+    document.querySelector("h1").textContent = "🚩 PLAYER 1 WINS";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").textContent = "PLAYER 2 WINS 🚩";
+}
+else{
+    document.querySelector("h1").textContent = "🚩 DRAW 🚩";
+}
